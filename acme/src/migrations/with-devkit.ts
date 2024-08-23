345750bda4a97a61ee4751079a439815dddacc9f
Guillaume Nury <gnury@luccasoftware.com>
2024-08-23 16:50:42 +0200

    add with-devkit

--- acme/src/migrations/with-devkit.ts
+++ acme/src/migrations/with-devkit.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+require('@angular-devkit/schematics');
+
 export default function version4(_options: any): Rule {
-  console.log('version4 called');
+  console.log('withDevkit: version4');
   return (host: Tree, context: SchematicContext) => {
     const version = `4.0.0`;
-    context.logger.info(`Applying migration for lib to version ${version}`);
+    context.logger.info(
+      `withDevkit: Applying migration for lib to version ${version}`
+    );
     return host;
   };
 }
